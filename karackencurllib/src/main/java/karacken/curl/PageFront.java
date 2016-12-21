72ca49ad0f2a1fe851881e44e312789871ecd97b
Grace Jang <gracej@kodalearning.com>
2016-12-20 16:57:30 -0800

    Test

--- karackencurllib/src/main/java/karacken/curl/PageFront.java
+++ karackencurllib/src/main/java/karacken/curl/PageFront.java
@@ -45,9 +45,9 @@ public class PageFront extends Page {
 
 				if(isactive())
 					vertices[pos+2]=(float) (calc_r*(Math.sin(3.14/(GRID*0.60f)*(col-(dx))))+(calc_r*1.1f)); //Asin(2pi/wav*x)
-				float w_h_ratio = (1-calc_r) * 1.01f;
+				float w_h_ratio = (1-calc_r) * 1.001f;
 
-					vertices[pos]=((float)col/(float)GRID*w_h_ratio)-mov_x-0.01f;
+					vertices[pos]=((float)col/(float)GRID*w_h_ratio)-mov_x-0.001f;
 					vertices[pos+1]=((float)row/(float)GRID*h_w_ratio)-h_w_correction;
 			}
 
